f16044cfde7d9fe6199536598075ff05d4559c14
Seipati Tshabalala <seitshab021@student.wethinkcode.co.za>
2023-03-05 10:34:52 +0200

    New work

---
+++ src/test/java/com/example/projectceleb/ProjectCelebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectceleb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCelebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
