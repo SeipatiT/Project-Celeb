2e9f0589814754f17d959f08748e4f12428ecf82
SeipatiT <109255052+SeipatiT@users.noreply.github.com>
2023-03-05 13:00:21 +0200

    New work

--- src/main/java/com/example/projectceleb/ProjectCelebApplication.java
+++ src/main/java/com/example/projectceleb/ProjectCelebApplication.java
@@ -3,6 +3,8 @@ package com.example.projectceleb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*Create database*/
+
 @SpringBootApplication
 public class ProjectCelebApplication {
 
